570998aa6931a112d5972a8c65cc4691cad6b803
QueenterJuma <jumahqueenter@gmail.com>
2023-02-23 15:40:19 +0300

    Add update in index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 
 const taskList = document.getElementById('taskList');
+const newTaskForm = document.getElementById('newTaskForm');
+const clearCompletedButton = document.getElementById('clearCompleted');
 let tasks = [];
 const saveTasks = () => {
   localStorage.setItem('tasks', JSON.stringify(tasks));
@@ -42,3 +44,74 @@ const renderTaskList = () => {
     taskList.appendChild(taskElement);
   });
 };
+
+const loadTasks = () => {
+  const tasksJson = localStorage.getItem('tasks');
+  if (tasksJson) {
+    tasks = JSON.parse(tasksJson);
+  }
+};
+
+const addTask = (name) => {
+  const task = { name, completed: false };
+  tasks.push(task);
+  saveTasks();
+  renderTaskList();
+};
+
+const clearCompletedTasks = () => {
+  tasks = tasks.filter((task) => !task.completed);
+  saveTasks();
+  renderTaskList();
+};
+
+const handleDragStart = (event) => {
+  event.target.classList.add('dragging');
+};
+
+const handleDragOver = (event) => {
+  event.preventDefault();
+  const taskElement = event.target.closest('.task');
+  const draggingElement = document.querySelector('.dragging');
+  if (taskElement && draggingElement && taskElement !== draggingElement) {
+    const taskElementRect = taskElement.getBoundingClientRect();
+    const dragY = event.clientY - taskElementRect.top;
+    const isAfter = dragY > taskElementRect.height / 2;
+    taskList.insertBefore(
+      draggingElement,
+      isAfter ? taskElement.nextSibling : taskElement,
+    );
+    const newIndex = Array.from(taskList.querySelectorAll('.task')).indexOf(
+      draggingElement,
+    );
+    const oldIndex = draggingElement.getAttribute('data-index');
+    tasks.splice(newIndex, 0, tasks.splice(oldIndex, 1)[0]);
+    saveTasks();
+  }
+};
+
+const handleDragEnd = (event) => {
+  event.target.classList.remove('dragging');
+};
+
+newTaskForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const newTaskInput = document.getElementById('newTask');
+  const taskName = newTaskInput.value.trim();
+  if (taskName) {
+    addTask(taskName);
+    newTaskInput.value = '';
+    newTaskInput.focus();
+  }
+});
+
+clearCompletedButton.addEventListener('click', () => {
+  clearCompletedTasks();
+});
+
+taskList.addEventListener('dragstart', handleDragStart);
+taskList.addEventListener('dragover', handleDragOver);
+taskList.addEventListener('dragend', handleDragEnd);
+
+loadTasks();
+renderTaskList();
